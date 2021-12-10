aeeaae54b043356a23ddd9aea157362db7823987
jhong40 <jhong40@gmail.com>
2021-12-10 08:51:08 -0500

    Update tool-install.sh

--- tool-install.sh
+++ tool-install.sh
@@ -9,6 +9,7 @@ aws --version
 
 ### install kubectl
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+chmod 755 kubectl
 sudo mv kubectl /usr/local/bin
 kubectl version
 
